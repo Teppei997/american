64a494d96c87fceb280cbb4b3b3273448e7b310e
北岡徹平 <teppei997@gmail.com>
2020-05-27 15:31:42 +0900

    sass導入

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+// // gulpfile.js：Gulpを使う時に必要なファイル
+//             この中にGulpにやってほしいことを書く
+// Gulp：タスクランナー
+// 今回作るタスク：scssをcssに変換するタスク
+
+// gulpを読み込み
+const gulp = require('gulp');
+
+// GulpでSassを使うためにgulp-sassを読み込む
+const sass = require('gulp-sass');
+
+// Gulpのタスクを作る
+// gulp.task('タスク名', function () {
+//   return (
+//     // タスクの内容
+//   )
+// })
+
+gulp.task('sass', function () {
+    return (
+        // sassフォルダのstyle .scsを取得
+      gulp.src('sass/style.scss')
+        .pipe(sass({
+            // outputStyle: 'compressed'
+        }))// 取得したscssをcssに変換
+        .pipe(gulp.dest('css'))//変換した結果をcssフォルダに出力
+    )
+  })
