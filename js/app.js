a233961a5becd3836e561a5c4320839870320c45
北岡徹平 <teppei997@gmail.com>
2020-05-31 23:21:23 +0900

    ファイル移動

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(function(){
+    $('body').bgSwitcher({
+        images: ['american/img/americanvillege.jpg', 'american/img/nightview.jpg', 'american/img/americancafe.jpg'],
+        interval: 2000,                         // 切り替えの間隔を指定します。
+        loop: true                            // 切り替えをループするかを指定します。
+    })
+
+
+})
+
